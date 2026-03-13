5435464392164c53067c569760ee9493b656933c
CosmicCat14 <cosmiccatnebula@icloud.com>
2026-03-13 13:38:16 -0500

    Fix offline page

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ const CACHE_NAME = 'offline';
 const OFFLINE_PAGE = 'https://cosmiccat14.github.io/commodity-values/offline.html';
 const OFFLINE_LOGO = '/img/app_icon_192.png';
 const OFFLINE_STYLE = 'https://cosmiccat14.github.io/commodity-values/style.css';
-const OFFLINE_FAVICON = '/favicon.ico';
-const FILES = [OFFLINE_PAGE,OFFLINE_LOGO, OFFLINE_STYLE, OFFLINE_FAVICON];
+const OFFLINE_REDIRECT = 'https://cosmiccat14.github.io/commodity-values/silver.html';
+const FILES = [OFFLINE_PAGE,OFFLINE_LOGO, OFFLINE_STYLE, OFFLINE_REDIRECT];
 
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installed.');
@@ -44,18 +44,18 @@ self.addEventListener('fetch', function(event) {
     } else {
         // Check if request is GET and from origin (commodity-values), then
         // hard check for logo and favicon in cache to support offline page
-        // if matches font url, serve cached font
+        // if matches style url, serve cached style
         if (event.request.method === 'GET') {
             const url = new URL(event.request.url);
             if (url.origin === self.location.origin) {
-                if (url.pathname === OFFLINE_LOGO || url.pathname === OFFLINE_FAVICON) {
+                if (url.pathname === OFFLINE_LOGO) {
                     return event.respondWith(
                         fetch(event.request).catch(() => caches.match(url.pathname))
                     )
                 }
-            } else if (event.request.url === OFFLINE_FONT) {
+            } else if (event.request.url === OFFLINE_STYLE) {
                 return event.respondWith(
-                    fetch(event.request).catch(() => caches.match(OFFLINE_FONT))
+                    fetch(event.request).catch(() => caches.match(OFFLINE_STYLE))
                 );
             }
         }
